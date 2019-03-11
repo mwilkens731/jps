6192bfd76ef023e9cf8a1e4a0ccb69ddf2782c6f
Matt Wilkens <matt.wilkens.u66w@statefarm.com>
2019-03-11 15:08:54 -0500

    no cors?

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Main from './Main';
-import {Auth} from './Auth';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
--- src/Main.js
+++ src/Main.js
@@ -12,23 +12,37 @@ class Main extends Component {
     this.state = {
       oauth: new OAuth.OAuth2(AppData.clientId, AppData.clientSecret, 'https://api.login.yahoo.com/', 'oauth2/request_auth', 'oauth2/get_token')
     };
+    this.saveLoginResults = this.saveLoginResults.bind(this);
   }
 
-  async componentDidMount (props) {
+  componentDidMount (props) {
     let code = (queryString.parse(this.props.location.search)).code;
-    if (!code) {
-      console.log('in', this.state.oauth.getAuthorizeUrl({
-        redirect_uri: 'http://mwilkens731.github.io/jps/',
-        response_type: 'code'
-      }));
+    if (!code && !this.state.code) {
       window.location.assign(this.state.oauth.getAuthorizeUrl({
-        redirect_uri: 'https://mwilkens731.github.io/jps/',
+        redirect_uri: 'https://mwilkens731.github.io/jps',
         response_type: 'code'
       }));
     } else {
-      console.log('in else');
-      console.log(code);
+      this.state.oauth.getOAuthAccessToken(code, {
+        grant_type: 'authorization_code',
+        redirect_uri: 'https://mwilkens731.github.io/jps'
+      }, this.saveLoginResults);
     }
+  }
+
+  saveLoginResults (e, code, access_token, refresh_token) {
+    if (e) {
+      console.log(e);
+    } else {
+      console.log('success!');
+      console.log('access', access_token);
+      console.log('refresh', refresh_token);
+    }
+    this.setState({
+      code: code,
+      accessToken: access_token,
+      refreshToken: refresh_token
+    });
   }
 
   render () {
