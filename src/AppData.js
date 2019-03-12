f0f6f9056c8d4f9591d014fce0ed613e5617189b
Matt Wilkens <matt.wilkens.u66w@statefarm.com>
2019-03-12 16:17:50 -0500

    progress

--- src/AppData.js
+++ src/AppData.js
@@ -4,5 +4,22 @@ export default {
   clientId: 'dj0yJmk9WW9hSlNxcDllcUZwJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTU5',
   clientSecret: '4a1c4d6e5af02bfe69e226d666a5113a7b2b35a6',
   corsAnywhereUrl: 'https://cors-anywhere.herokuapp.com/',
-  leagueKey: 'mlb.l.74981'
+  leagueKeys: {
+    thisYear: '388.l.74981',
+    lastYear: '378.l.46962'
+  },
+  lastYearTeamKeys: {
+    1: '.t.1',
+    2: '.t.12',
+    3: '.t.2',
+    4: '.t.4',
+    5: '.t.5',
+    6: '.t.6',
+    7: '.t.7',
+    8: '.t.11',
+    9: '.t.8',
+    10: '.t.9',
+    11: '.t.10',
+    12: '.t.3'
+  }
 };
--- src/Main.js
+++ src/Main.js
@@ -4,32 +4,57 @@ import './App.css';
 import AppData from './AppData';
 import { withRouter } from 'react-router';
 import queryString from 'query-string';
+import axios from 'axios';
 var OAuth = require('oauth');
 
 class Main extends Component {
   constructor (props) {
     super(props);
+    let redirectUri = 'https://mwilkens731.github.io/jps';
+    if (window.location.host.indexOf('localhost') >= 0) {
+      redirectUri = 'oob';
+    }
     this.state = {
       oauth: new OAuth.OAuth2(AppData.clientId, AppData.clientSecret, 'https://api.login.yahoo.com/', 'oauth2/request_auth', 'oauth2/get_token'),
-      oauthWithProxy: new OAuth.OAuth2(AppData.clientId, AppData.clientSecret, AppData.corsAnywhereUrl + 'https://api.login.yahoo.com/', 'oauth2/request_auth', 'oauth2/get_token')
+      oauthWithProxy: new OAuth.OAuth2(AppData.clientId, AppData.clientSecret, AppData.corsAnywhereUrl + 'https://api.login.yahoo.com/', 'oauth2/request_auth', 'oauth2/get_token'),
+      redirectUri: redirectUri,
+      loading: true
     };
     this.state.oauthWithProxy.useAuthorizationHeaderforGET(true);
     this.saveLoginResults = this.saveLoginResults.bind(this);
-    this.saveTeams = this.saveTeams.bind(this);
+    this.extractTeams = this.extractTeams.bind(this);
+    this.getAxiosHeaders = this.getAxiosHeaders.bind(this);
+    this.getRosters = this.getRosters.bind(this);
   }
 
   async componentDidMount (props) {
     let code = (queryString.parse(this.props.location.search)).code;
     if (!code && !this.state.code) {
       window.location.assign(this.state.oauth.getAuthorizeUrl({
-        redirect_uri: 'https://mwilkens731.github.io/jps',
+        redirect_uri: this.state.redirectUri,
         response_type: 'code'
       }));
     } else {
       this.state.oauthWithProxy.getOAuthAccessToken(code, {
         grant_type: 'authorization_code',
-        redirect_uri: 'https://mwilkens731.github.io/jps'
+        redirect_uri: this.state.redirectUri
       }, this.saveLoginResults);
+    }
+  }
+
+  getAxiosHeaders (accessToken) {
+    if (!accessToken) {
+      return {
+        headers: {
+          'Authorization': 'Bearer ' + this.state.accessToken
+        }
+      };
+    } else {
+      return {
+        headers: {
+          'Authorization': 'Bearer ' + accessToken
+        }
+      };
     }
   }
 
@@ -37,27 +62,63 @@ class Main extends Component {
     if (e) {
       console.log(e);
     } else {
-      console.log('success!');
-      console.log('access', access_token);
-      console.log('refresh', refresh_token);
-      let result = await this.state.oauthWithProxy.get(AppData.corsAnywhereUrl + 'https://fantasysports.yahooapis.com/fantasy/v2/league/' + AppData.leagueKey + '/teams?format=json', refresh_token.access_token, this.saveTeams);
-      console.log('save result', result);
+      this.setState({
+        code: code,
+        accessToken: refresh_token.access_token
+      });
+      let result = await axios.get(AppData.corsAnywhereUrl + 'https://fantasysports.yahooapis.com/fantasy/v2/league/' + AppData.leagueKeys.thisYear + '/teams?format=json', this.getAxiosHeaders(refresh_token.access_token));
+      this.extractTeams(result.data.fantasy_content.league[1].teams, result.data.fantasy_content.league[0].draft_status === 'predraft');
     }
-    this.setState({
-      code: code,
-      accessToken: refresh_token.access_token
-    });
   }
 
-  saveTeams(response){
-    console.log('response', response);
+  extractTeams (teams, predraft) {
+    console.log('teams', teams);
+    let teamsArray = [];
+    for (let i = 0; i < teams.count; i++) {
+      let thisTeam = teams[i];
+      teamsArray.push({
+        name: thisTeam.team[0][2].name,
+        teamKey: thisTeam.team[0][0].team_key,
+        lastYearTeamKey: AppData.leagueKeys.lastYear + AppData.lastYearTeamKeys[thisTeam.team[0][1].team_id]
+      });
+    }
+    console.log('teamsArray', teamsArray);
+    return this.getRosters(teamsArray, predraft);
+  }
+
+  getRosters (teams, predraft) {
+    teams.forEach(async (team) => {
+      let result = await axios.get(
+        AppData.corsAnywhereUrl + 'https://fantasysports.yahooapis.com/fantasy/v2/team/' + (predraft ? team.lastYearTeamKey : team.teamKey) + '/roster?format=json',
+        this.getAxiosHeaders(this.state.accessToken));
+      console.log('roster results', result.data.fantasy_content.team[1].roster[0].players);
+    });
+    this.setState({
+      loading: false
+    });
   }
 
   render () {
     return (
-      <div>
-        <div>Hi</div>
-        <div>{this.state.accessToken}</div>
+      <div className='container-fluid'>
+        <div className='row'>
+          <h1 className='col-12 jumbotron text-center'>Juan Pierre Sucks Keeper Costs</h1>
+        </div>
+        {this.state.loading &&
+          <div>
+            <div className='spinner-grow' style={{width: '10rem', height: '10rem'}} role='status'>
+              <span class='sr-only'>Loading...</span>
+            </div>
+            <div className='row'>
+              <h5 className='col-12 text-center'>Loading Yahoo data...</h5>
+            </div>
+          </div>
+
+        }
+        {!this.state.loading &&
+          <div>{this.state.accessToken}</div>
+        }
+
       </div>
     );
   }
