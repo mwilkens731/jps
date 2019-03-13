34a73c63a8d7a1c90681f866977de3006fca8c4e
Matt Wilkens <matt.wilkens.u66w@statefarm.com>
2019-03-13 15:34:23 -0500

    more

---
+++ src/Keepers.js
@@ -0,0 +1,372 @@
+export default [
+  {
+    name: 'Ryan Madson',
+    cost: 17,
+    year: 2
+  },
+  {
+    name: 'Javier Baez',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Rich Hill',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Kris Bryant',
+    cost: 1,
+    year: 2
+  },
+  {
+    name: 'J.D. Martinez',
+    cost: 3,
+    year: 2
+  },
+  {
+    name: 'Aaron Nola',
+    cost: 9,
+    year: 2
+  },
+  {
+    name: 'Collin McHugh',
+    cost: 16,
+    year: 2
+  },
+  {
+    name: 'Josh Bell',
+    cost: 22,
+    year: 2
+  },
+  {
+    name: 'Yuli Gurriel',
+    cost: 23,
+    year: 2
+  },
+  {
+    name: 'Freddie Freeman',
+    cost: 1,
+    year: 2
+  },
+  {
+    name: 'Trevor Story',
+    cost: 13,
+    year: 3
+  },
+  {
+    name: 'Jose Quintana',
+    cost: 4,
+    year: 2
+  },
+  {
+    name: 'Lucas Giolito',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Brad Hand',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Aaron Judge',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Mike Trout',
+    cost: 1,
+    year: 3
+  },
+  {
+    name: 'Craig Kimbrel',
+    cost: 6,
+    year: 2
+  },
+  {
+    name: 'James Paxton',
+    cost: 7,
+    year: 2
+  },
+  {
+    name: 'Mike Moustakas',
+    cost: 18,
+    year: 2
+  },
+  {
+    name: 'Nicholas Castellanos',
+    cost: 21,
+    year: 2
+  },
+  {
+    name: 'Carlos Rodon',
+    cost: 10,
+    year: 2
+  },
+  {
+    name: 'Reynaldo Lopez',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Alex Wood',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Bryce Harper',
+    cost: 1,
+    year: 2
+  },
+  {
+    name: 'Stephen Strasburg',
+    cost: 2,
+    year: 2
+  },
+  {
+    name: 'Yoan Moncada',
+    cost: 17,
+    year: 2
+  },
+  {
+    name: 'Max Scherzer',
+    cost: 1,
+    year: 2
+  },
+  {
+    name: 'Carlos Carrasco',
+    cost: 2,
+    year: 2
+  },
+  {
+    name: 'Christian Yelich',
+    cost: 4,
+    year: 3
+  },
+  {
+    name: 'Anthony DeSclafani',
+    cost: 13,
+    year: 2
+  },
+  {
+    name: 'Ian Happ',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Matt Olson',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Zack Greinke',
+    cost: 4,
+    year: 2
+  },
+  {
+    name: 'Yu Darvish',
+    cost: 8,
+    year: 3
+  },
+  {
+    name: 'Ozzie Albies',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Michael Conforto',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Zack Godley',
+    cost: 17,
+    year: 2
+  },
+  {
+    name: 'Paul Goldschmidt',
+    cost: 1,
+    year: 3
+  },
+  {
+    name: 'A.J. Pollock',
+    cost: 1,
+    year: 2
+  },
+  {
+    name: 'Gerrit Cole',
+    cost: 4,
+    year: 2
+  },
+  {
+    name: 'Masahiro Tanaka',
+    cost: 6,
+    year: 3
+  },
+  {
+    name: 'Dallas Keuchel',
+    cost: 7,
+    year: 2
+  },
+  {
+    name: 'Daniel Murphy',
+    cost: 9,
+    year: 3
+  },
+  {
+    name: 'Marcell Ozuna',
+    cost: 12,
+    year: 2
+  },
+  {
+    name: 'Yasiel Puig',
+    cost: 18,
+    year: 2
+  },
+  {
+    name: 'Scooter Gennett',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Chris Taylor',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Nolan Arenado',
+    cost: 1,
+    year: 2
+  },
+  {
+    name: 'Robbie Ray',
+    cost: 11,
+    year: 2
+  },
+  {
+    name: 'Alex Bregman',
+    cost: 23,
+    year: 3
+  },
+  {
+    name: 'Alex Colome',
+    cost: 23,
+    year: 3
+  },
+  {
+    name: 'Cody Bellinger',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Corey Knebel',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Felipe Vazquez',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Jacob deGrom',
+    cost: 2,
+    year: 2
+  },
+  {
+    name: 'Andrew Benintendi',
+    cost: 23,
+    year: 3
+  },
+  {
+    name: 'Gary Sanchez',
+    cost: 23,
+    year: 3
+  },
+  {
+    name: 'Jose Berrios',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Joey Gallo',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Rhys Hoskins',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Tommy Pham',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Luis Severino',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Ken Giles',
+    cost: 9,
+    year: 2
+  },
+  {
+    name: 'Jeff Samardzija',
+    cost: 9,
+    year: 2
+  },
+  {
+    name: 'Adam Duvall',
+    cost: 23,
+    year: 3
+  },
+  {
+    name: 'Pedro Baez',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Justin Bour',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Rafael Devers',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Carson Fulmer',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Austin Hedges',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Lance Lynn',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Jorge Polanco',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Anthony Swarzak',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Ryan Zimmerman',
+    cost: 24,
+    year: 2
+  }
+];
--- src/Main.js
+++ src/Main.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './App.css';
 import AppData from './AppData';
+import Keepers from './Keepers';
 import { withRouter } from 'react-router';
 import queryString from 'query-string';
 import axios from 'axios';
@@ -18,8 +19,11 @@ class Main extends Component {
       teamsRetrieved: false,
       draftResultsRetrieved: false,
       rostersRetrieved: 0,
+      rosterEnhancementStatus: 0,
+      freeAgentEnhancementStatus: 0,
       isLocalHost: window.location.host.indexOf('localhost') === 0,
-      teams: []
+      teams: [],
+      freeAgents: []
     };
     this.state.oauthWithProxy.useAuthorizationHeaderforGET(true);
     this.saveLoginResults = this.saveLoginResults.bind(this);
@@ -29,6 +33,11 @@ class Main extends Component {
     this.getLoadingStatus = this.getLoadingStatus.bind(this);
     this.redirectToYahooAuth = this.redirectToYahooAuth.bind(this);
     this.getDraftResults = this.getDraftResults.bind(this);
+    this.populateKeeperData = this.populateKeeperData.bind(this);
+    this.populateFreeAgents = this.populateFreeAgents.bind(this);
+    this.transformPlayer = this.transformPlayer.bind(this);
+    this.enhancePlayer = this.enhancePlayer.bind(this);
+    console.log('keepers in file: ' + Keepers.length);
   }
 
   async componentDidMount (props) {
@@ -36,14 +45,14 @@ class Main extends Component {
     if (!code && !this.state.code) {
       this.redirectToYahooAuth();
     } else {
-        this.state.oauthWithProxy.getOAuthAccessToken(code, {
-          grant_type: 'authorization_code',
-          redirect_uri: this.state.redirectUri
-        }, this.saveLoginResults);
+      this.state.oauthWithProxy.getOAuthAccessToken(code, {
+        grant_type: 'authorization_code',
+        redirect_uri: this.state.redirectUri
+      }, this.saveLoginResults);
     }
   }
 
-  redirectToYahooAuth(){
+  redirectToYahooAuth () {
     window.location.assign(this.state.oauth.getAuthorizeUrl({
       redirect_uri: this.state.redirectUri,
       response_type: 'code'
@@ -68,7 +77,7 @@ class Main extends Component {
 
   async saveLoginResults (e, code, access_token, refresh_token) {
     if (e && e.statusCode === 400 && !this.state.isLocalHost) {
-        this.redirectToYahooAuth();
+      this.redirectToYahooAuth();
     } else {
       this.setState({
         code: code,
@@ -95,47 +104,17 @@ class Main extends Component {
         roster: []
       });
     }
-    console.log('teamsArray', teamsArray);
     this.getDraftResults();
     this.getRosters(teamsArray);
   }
 
-  getRosters (teams) {
-    teams.forEach(async (team) => {
-      let result = await axios.get(
-        AppData.corsAnywhereUrl + 'https://fantasysports.yahooapis.com/fantasy/v2/team/' + (this.state.predraft ? team.lastYearTeamKey : team.teamKey) + '/roster?format=json',
-        this.getAxiosHeaders(this.state.accessToken));
-      let thisRoster = result.data.fantasy_content.team[1].roster[0].players;
-      for(let i = 0; i < thisRoster.count; i++){
-        let thisPlayer = thisRoster[i].player[0];
-        let displayPostionIndex = 8;
-        if(!thisPlayer[8].display_position){
-          displayPostionIndex++;
-          if(!thisPlayer[9].display_position){
-            console.log('undefined player position for ', !thisPlayer[8].display_position);
-          }
-        }
-        team.roster.push({
-          playerKey: thisPlayer[0].player_key,
-          name: thisPlayer[2].name.full,
-          position: thisPlayer[displayPostionIndex].display_position
-        });
-      }
-      this.setState({
-        rostersRetrieved: this.state.rostersRetrieved + 1,
-        teams: [...this.state.teams, team]
-      });
-    });
-
-  }
-
-  async getDraftResults(){
+  async getDraftResults () {
     let results = await axios.get(
       AppData.corsAnywhereUrl + 'https://fantasysports.yahooapis.com/fantasy/v2/league/' + (this.state.predraft ? AppData.leagueKeys.lastYear : AppData.leagueKeys.thisYear) + '/draftresults?format=json',
         this.getAxiosHeaders(this.state.accessToken));
-    let extractedResults = results.data.fantasy_content.league[1].draft_results
+    let extractedResults = results.data.fantasy_content.league[1].draft_results;
     let draftResults = [];
-    for(let i = 0; i < extractedResults.count; i++){
+    for (let i = 0; i < extractedResults.count; i++) {
       draftResults.push({
         round: extractedResults[i].draft_result.round,
         playerKey: extractedResults[i].draft_result.player_key
@@ -148,16 +127,130 @@ class Main extends Component {
     });
   }
 
-  getLoadingStatus(){
-    if (!this.state.authenticated){
+  async getRosters (teams) {
+    let commaSeparatedTeamKeys = '';
+    teams.forEach((team) => {
+      if (commaSeparatedTeamKeys.length === 0) {
+        commaSeparatedTeamKeys = (this.state.predraft ? team.lastYearTeamKey : team.teamKey);
+      } else {
+        commaSeparatedTeamKeys += ',' + (this.state.predraft ? team.lastYearTeamKey : team.teamKey);
+      }
+    });
+    console.log('temas in ', teams);
+
+    let result = await axios.get(
+      AppData.corsAnywhereUrl + 'https://fantasysports.yahooapis.com/fantasy/v2/teams;team_keys=' + commaSeparatedTeamKeys + '/roster?format=json',
+      this.getAxiosHeaders(this.state.accessToken));
+    console.log('teams reulsts', result);
+    for (let i = 0; i < teams.length; i++) {
+    // teams.forEach((team) => {
+      // let result = await axios.get(
+      //   AppData.corsAnywhereUrl + 'https://fantasysports.yahooapis.com/fantasy/v2/team/' + (this.state.predraft ? team.lastYearTeamKey : team.teamKey) + '/roster?format=json',
+      //   this.getAxiosHeaders(this.state.accessToken));
+      let thisRoster = result.data.fantasy_content.teams[i].team[1].roster[0].players;
+      for (let j = 0; j < thisRoster.count; j++) {
+        teams[i].roster.push(this.transformPlayer(thisRoster[j].player[0]));
+      }
+      this.setState({
+        rostersRetrieved: this.state.rostersRetrieved + 1,
+        teams: [...this.state.teams, teams[i]]
+      });
+    // });
+    }
+  }
+
+  transformPlayer (thisPlayer) {
+    let displayPostionIndex = 8;
+    if (!thisPlayer[8].display_position) {
+      displayPostionIndex++;
+      if (!thisPlayer[9].display_position) {
+        console.log('undefined player position for ', !thisPlayer[8].display_position);
+      }
+    }
+    return {
+      playerKey: thisPlayer[0].player_key,
+      name: thisPlayer[2].name.ascii_first + ' ' + thisPlayer[2].name.ascii_last,
+      position: thisPlayer[displayPostionIndex].display_position
+    };
+  }
+
+  populateKeeperData () {
+    this.setState({
+      rosterEnhancementStatus: 1
+    });
+    let keepers = 0;
+    this.state.teams.forEach((thisTeam) => {
+      thisTeam.roster.forEach((player) => {
+        if (this.enhancePlayer(player)) {
+          keepers++;
+        }
+      });
+    });
+    this.setState({
+      rosterEnhancementStatus: 2
+    });
+    console.log('populated data', this.state.teams);
+    console.log('keepers on rosters: ', keepers);
+  }
+
+  async populateFreeAgents () {
+    this.setState({
+      freeAgentEnhancementStatus: 1
+    });
+    let keepers = 0;
+    console.log('draft results in populate', this.state.draftResults);
+    let freeAgents = this.state.draftResults.filter(pick => !pick.name);
+    console.log('free agents to populate: ', freeAgents.length);
+    let commaSeparatedPlayersKeys = '';
+    freeAgents.forEach((thisFA) => {
+      if (commaSeparatedPlayersKeys.length === 0) {
+        commaSeparatedPlayersKeys = thisFA.playerKey;
+      } else {
+        commaSeparatedPlayersKeys += ',' + thisFA.playerKey;
+      }
+    });
+    let results = await axios.get(
+      AppData.corsAnywhereUrl + 'https://fantasysports.yahooapis.com/fantasy/v2/players;player_keys=' + commaSeparatedPlayersKeys + '?format=json',
+      this.getAxiosHeaders(this.state.accessToken));
+    console.log('results', results);
+    console.log('comma', commaSeparatedPlayersKeys);
+  }
+
+  enhancePlayer (player) {
+    let draftResult = this.state.draftResults.find(pick => pick.playerKey === player.playerKey);
+    let draftRound = 26;
+    if (draftResult) {
+      draftResult.name = player.name;
+      draftResult.position = player.position;
+      draftRound = draftResult.round;
+    }
+    let keeper = Keepers.find(k => k.name === player.name);
+    if (keeper) {
+      player.cost = keeper.cost;
+      player.year = keeper.year;
+    } else {
+      player.cost = draftRound === 1 ? 1 : draftRound - 1;
+      player.year = 1;
+    }
+    return keeper;
+  }
+
+  getLoadingStatus () {
+    if (!this.state.authenticated) {
       return 'Authenticating';
     } else if (!this.state.teamsRetrieved) {
-      return 'Retrieving Team List...'
+      return 'Retrieving Team List...';
     } else if (!this.state.draftResultsRetrieved) {
       return 'Retrieving Draft Results...';
-    }
-    else if (this.state.rostersRetrieved < 12) {
+    } else if (this.state.rostersRetrieved < 12) {
       return `Retrieving Roster (${this.state.rostersRetrieved} / 12)`;
+    } else if (this.state.rosterEnhancementStatus !== 2) {
+      if (!this.state.rosterEnhancementStatus) {
+        this.populateKeeperData();
+      }
+      return 'Populating Keeper Data...';
+    } else if (!this.state.freeAgentEnhancementStatus) {
+      this.populateFreeAgents();
     }
     return 'Done';
   }
@@ -174,15 +267,22 @@ class Main extends Component {
               <span className='sr-only'>Loading...</span>
             </div>
             <div className='row'>
-              <h5 className='col-12 text-center'>{this.getLoadingStatus()}...</h5>
+              <h5 className='col-12 text-center'>{this.getLoadingStatus()}</h5>
             </div>
           </div>
 
         }
-        {!this.state.loading &&
-          <div>{this.state.accessToken}</div>
+        {this.getLoadingStatus() === 'Done' &&
+          <div className='row'>
+            <div className='col-12 col-m-6'>
+              Hi 1
+            </div>
+            <div className='col-12 col-m-6'>
+              Hi 2
+            </div>
+            {this.state.accessToken}
+          </div>
         }
-
       </div>
     );
   }
