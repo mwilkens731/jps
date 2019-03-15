720970f6c9b833fd872760c538a773ca69d6b1a8
Matt Wilkens <matt.wilkens.u66w@statefarm.com>
2019-03-14 22:19:27 -0500

    next years keepers

---
+++ src/NextYearKeepers.js
@@ -0,0 +1,382 @@
+export default [
+  {
+    name: 'Jose Ramirez',
+    cost: 4,
+    year: 2
+  },
+  {
+    name: 'Charlie Morton',
+    cost: 12,
+    year: 2
+  },
+  {
+    name: 'Jackie Bradley Jr.',
+    cost: 12,
+    year: 2
+  },
+  {
+    name: 'Javier Baez',
+    cost: 23,
+    year: 3
+  },
+  {
+    name: 'Rich Hill',
+    cost: 23,
+    year: 3
+  },
+  {
+    name: 'Chris Devenski',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'J.D. Martinez',
+    cost: 2,
+    year: 3
+  },
+  {
+    name: 'Aaron Nola',
+    cost: 8,
+    year: 3
+  },
+  {
+    name: 'Blake Snell',
+    cost: 11,
+    year: 2
+  },
+  {
+    name: 'David Peralta',
+    cost: 18,
+    year: 2
+  },
+  {
+    name: 'Josh Bell',
+    cost: 21,
+    year: 3
+  },
+  {
+    name: 'Yuli Gurriel',
+    cost: 22,
+    year: 3
+  },
+  {
+    name: 'Marcus Semien',
+    cost: 23,
+    year: 2
+  },
+  {
+    name: 'Mookie Betts',
+    cost: 1,
+    year: 2
+  },
+  {
+    name: 'Trevor Bauer',
+    cost: 10,
+    year: 2
+  },
+  {
+    name: 'Victor Robles',
+    cost: 16,
+    year: 2
+  },
+  {
+    name: 'Vladimir Guerrero Jr.',
+    cost: 17,
+    year: 2
+  },
+  {
+    name: 'Eloy Jimenez',
+    cost: 18,
+    year: 2
+  },
+  {
+    name: 'Kyle Tucker',
+    cost: 21,
+    year: 2
+  },
+  {
+    name: 'Yasmani Grandal',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Patrick Corbin',
+    cost: 16,
+    year: 2
+  },
+  {
+    name: 'Brad Hand',
+    cost: 23,
+    year: 3
+  },
+  {
+    name: 'Aaron Judge',
+    cost: 23,
+    year: 3
+  },
+  {
+    name: 'Jesus Aguilar',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Seranthony Dominguez',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Nathan Eovaldi',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Jose Peraza',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Ronald Acuna Jr.',
+    cost: 6,
+    year: 2
+  },
+  {
+    name: 'Carlos Rodon',
+    cost: 9,
+    year: 3
+  },
+  {
+    name: 'Tim Anderson',
+    cost: 12,
+    year: 2
+  },
+  {
+    name: 'Alex Wood',
+    cost: 23,
+    year: 3
+  },
+  {
+    name: 'Miguel Andujar',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Caleb Smith',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Kirby Yates',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Bryce Harper',
+    cost: 1,
+    year: 3
+  },
+  {
+    name: 'Josh Hader',
+    cost: 9,
+    year: 2
+  },
+  {
+    name: 'David Robertson',
+    cost: 10,
+    year: 2
+  },
+  {
+    name: 'Yoan Moncada',
+    cost: 16,
+    year: 3
+  },
+  {
+    name: 'Matt Chapman',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Joc Pederson',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Nick Pivetta',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Max Scherzer',
+    cost: 1,
+    year: 3
+  },
+  {
+    name: 'Edwin Diaz',
+    cost: 5,
+    year: 2
+  },
+  {
+    name: 'Eugenio Suarez',
+    cost: 14,
+    year: 2
+  },
+  {
+    name: 'Matt Olson',
+    cost: 23,
+    year: 3
+  },
+  {
+    name: 'Jack Flaherty',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Jose Leclerc',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'German Marquez',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Eddie Rosario',
+    cost: 12,
+    year: 2
+  },
+  {
+    name: 'Ozzie Albies',
+    cost: 23,
+    year: 3
+  },
+  {
+    name: 'Michael Conforto',
+    cost: 23,
+    year: 3
+  },
+  {
+    name: 'Michael Brantley',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Mike Foltynewicz',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Kyle Freeland',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Mallex Smith',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'A.J. Pollock',
+    cost: 1,
+    year: 3
+  },
+  {
+    name: 'Anthony Rendon',
+    cost: 1,
+    year: 2
+  },
+  {
+    name: 'Kenley Jansen',
+    cost: 2,
+    year: 2
+  },
+  {
+    name: 'Gerrit Cole',
+    cost: 3,
+    year: 3
+  },
+  {
+    name: 'Andrew McCutchen',
+    cost: 4,
+    year: 2
+  },
+  {
+    name: 'J.T. Realmuto',
+    cost: 9,
+    year: 2
+  },
+  {
+    name: 'Cole Hamels',
+    cost: 11,
+    year: 2
+  },
+  {
+    name: 'Nolan Arenado',
+    cost: 1,
+    year: 3
+  },
+  {
+    name: 'Khris Davis',
+    cost: 3,
+    year: 2
+  },
+  {
+    name: 'Lorenzo Cain',
+    cost: 6,
+    year: 2
+  },
+  {
+    name: 'Robbie Ray',
+    cost: 10,
+    year: 3
+  },
+  {
+    name: 'Gleyber Torres',
+    cost: 16,
+    year: 2
+  },
+  {
+    name: 'Cody Bellinger',
+    cost: 23,
+    year: 3
+  },
+  {
+    name: 'Felipe Vazquez',
+    cost: 23,
+    year: 3
+  },
+  {
+    name: 'Jacob deGrom',
+    cost: 1,
+    year: 3
+  },
+  {
+    name: 'Whit Merrifield',
+    cost: 3,
+    year: 2
+  },
+  {
+    name: 'Walker Buehler',
+    cost: 19,
+    year: 2
+  },
+  {
+    name: 'Jose Berrios',
+    cost: 23,
+    year: 3
+  },
+  {
+    name: 'Rhys Hoskins',
+    cost: 23,
+    year: 3
+  },
+  {
+    name: 'Luis Severino',
+    cost: 23,
+    year: 3
+  },
+  {
+    name: 'Juan Soto',
+    cost: 24,
+    year: 2
+  }
+];
