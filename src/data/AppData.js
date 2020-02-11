4d3f603628bc1ba4d2f182be03921864b207588c
Matt Wilkens <matthew.j.wilkens@gmail.com>
2020-02-10 20:06:50 -0600

    updates for 2020

--- src/data/AppData.js
+++ src/data/AppData.js
@@ -5,21 +5,21 @@ export default {
   clientSecret: '4a1c4d6e5af02bfe69e226d666a5113a7b2b35a6',
   corsAnywhereUrl: 'https://cors-anywhere.herokuapp.com/',
   leagueKeys: {
-    thisYear: '388.l.74981',
-    lastYear: '378.l.46962'
+    lastYear: '388.l.74981',
+    thisYear: '398.l.5072'
   },
   lastYearTeamKeys: {
-    1: '.t.1',
-    2: '.t.12',
-    3: '.t.2',
-    4: '.t.4',
-    5: '.t.5',
-    6: '.t.6',
-    7: '.t.7',
-    8: '.t.11',
-    9: '.t.8',
-    10: '.t.9',
-    11: '.t.10',
-    12: '.t.3'
+    1: '.t.1', //Pork
+    2: '.t.2',  //Anal
+    3: '.t.3', //Angry
+    4: '.t.4', //clever
+    5: '.t.5', //dubya
+    6: '.t.6', //vasta
+    7: '.t.7', //uribe
+    8: '.t.8', //joe
+    9: '.t.12', //shenanigans
+    10: '.t.9', //slump
+    11: '.t.10', //gary
+    12: '.t.11' //joe
   }
 };
