6644ebfe6fad69be64a6da94d36932b9b9e71e81
Matt Wilkens <matthew.j.wilkens@gmail.com>
2020-03-13 11:20:13 -0500

    first batch of 2020 keepers

--- src/data/NextYearKeepers.js
+++ src/data/NextYearKeepers.js
@@ -1,2 +1,333 @@
 export default [
+  {
+    name: 'Francisco Lindor',
+    cost: 1,
+    year: 1
+  },
+  {
+    name: 'Eddie Rosario',
+    cost: 12,
+    year: 2
+  },
+  {
+    name: 'J.D. Davis',
+    cost: 25,
+    year: 1
+  },
+  {
+    name: 'Shohei Ohtani (Pitcher)',
+    cost: 15,
+    year: 1
+  },
+  {
+    name: 'Frankie Montas',
+    cost: 25,
+    year: 1
+  },
+  {
+    name: 'Brandon Workman',
+    cost: 25,
+    year: 1
+  },
+  {
+    name: 'A.J. Puk',
+    cost: 25,
+    year: 1
+  },
+  {
+    name: 'Anthony Rendon',
+    cost: 1,
+    year: 2
+  },
+  {
+    name: 'Austin Meadows',
+    cost: 21,
+    year: 1
+  },
+  {
+    name: 'J.T. Realmuto',
+    cost: 9,
+    year: 2
+  },
+  {
+    name: 'DJ LeMahieu',
+    cost: 22,
+    year: 1
+  },
+  {
+    name: 'Nelson Cruz',
+    cost: 5,
+    year: 1
+  },
+  {
+    name: 'Vladimir Guerrero Jr.',
+    cost: 17,
+    year: 2
+  },
+  {
+    name: 'Victor Robles',
+    cost: 16,
+    year: 2
+  },
+  {
+    name: 'Eloy Jimenez',
+    cost: 18,
+    year: 2
+  },
+  {
+    name: 'Yasmani Grandal',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Didi Gregorius',
+    cost: 19,
+    year: 1
+  },
+  {
+    name: 'Nick Senzel',
+    cost: 11,
+    year: 1
+  },
+  {
+    name: 'Julio Urias',
+    cost: 25,
+    year: 1
+  },
+  {
+    name: 'Ketel Marte',
+    cost: 20,
+    year: 1
+  },
+  {
+    name: 'Matt Chapman',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Kyle Schwarber',
+    cost: 13,
+    year: 1
+  },
+  {
+    name: 'Jo Adell',
+    cost: 25,
+    year: 1
+  },
+  {
+    name: 'Shane Bieber',
+    cost: 7,
+    year: 1
+  },
+  {
+    name: 'Clayton Kershaw',
+    cost: 5,
+    year: 1
+  },
+  {
+    name: 'Josh Hader',
+    cost: 9,
+    year: 2
+  },
+  {
+    name: 'Pete Alonso',
+    cost: 16,
+    year: 1
+  },
+  {
+    name: 'Franmil Reyes',
+    cost: 18,
+    year: 1
+  },
+  {
+    name: 'Jorge Polanco',
+    cost: 25,
+    year: 1
+  },
+  {
+    name: 'Bryan Reynolds',
+    cost: 25,
+    year: 1
+  },
+  {
+    name: 'Cavan Biggio',
+    cost: 25,
+    year: 1
+  },
+  {
+    name: 'Jack Flaherty',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Chris Paddack',
+    cost: 15,
+    year: 1
+  },
+  {
+    name: 'Trevor Story',
+    cost: 1,
+    year: 1
+  },
+  {
+    name: 'Xander Bogaerts',
+    cost: 3,
+    year: 1
+  },
+  {
+    name: 'Jeff McNeil',
+    cost: 25,
+    year: 1
+  },
+  {
+    name: 'Yu Darvish',
+    cost: 7,
+    year: 1
+  },
+  {
+    name: 'Marcus Stroman',
+    cost: 18,
+    year: 1
+  },
+  {
+    name: 'Christian Yelich',
+    cost: 1,
+    year: 1
+  },
+  {
+    name: 'Marcus Semien',
+    cost: 23,
+    year: 2
+  },
+  {
+    name: 'Danny Santana',
+    cost: 25,
+    year: 1
+  },
+  {
+    name: 'Blake Snell',
+    cost: 11,
+    year: 2
+  },
+  {
+    name: 'Mike Clevinger',
+    cost: 4,
+    year: 1
+  },
+  {
+    name: 'Brandon Woodruff',
+    cost: 25,
+    year: 1
+  },
+  {
+    name: 'Zac Gallen',
+    cost: 25,
+    year: 1
+  },
+  {
+    name: 'Ronald Acuna Jr.',
+    cost: 6,
+    year: 2
+  },
+  {
+    name: 'Rafael Devers',
+    cost: 7,
+    year: 1
+  },
+  {
+    name: 'Tim Anderson',
+    cost: 12,
+    year: 2
+  },
+  {
+    name: 'Jorge Soler',
+    cost: 20,
+    year: 1
+  },
+  {
+    name: 'Kirby Yates',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Caleb Smith',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Dylan Cease',
+    cost: 25,
+    year: 1
+  },
+  {
+    name: 'Juan Soto',
+    cost: 24,
+    year: 2
+  },
+  {
+    name: 'Keston Hiura',
+    cost: 25,
+    year: 1
+  },
+  {
+    name: 'Bo Bichette',
+    cost: 25,
+    year: 1
+  },
+  {
+    name: 'Mitch Garver',
+    cost: 25,
+    year: 1
+  },
+  {
+    name: 'Walker Buehler',
+    cost: 19,
+    year: 2
+  },
+  {
+    name: 'Lucas Giolito',
+    cost: 25,
+    year: 1
+  },
+  {
+    name: 'Tyler Glasnow',
+    cost: 9,
+    year: 1
+  },
+  {
+    name: 'Yordan Alvarez',
+    cost: 25,
+    year: 1
+  },
+  {
+    name: 'Luis Robert',
+    cost: 25,
+    year: 1
+  },
+  {
+    name: 'Eduardo Escobar',
+    cost: 14,
+    year: 1
+  },
+  {
+    name: 'Patrick Corbin',
+    cost: 16,
+    year: 2
+  },
+  {
+    name: 'Mike Soroka',
+    cost: 25,
+    year: 1
+  },
+  {
+    name: 'Max Fried',
+    cost: 25,
+    year: 1
+  },
+  {
+    name: 'Dakota Hudson',
+    cost: 25,
+    year: 1
+  }
+
 ];
