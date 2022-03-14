bbc893dc57d48254c09346458e9cf7d60231e1a9
mwilkens731 <matthew.j.wilkens@gmail.com>
2022-03-13 19:43:18 -0500

    Updates

--- precache-manifest.85d8e487ce7915635d25b5600a162dd6.js
+++ precache-manifest.85d8e487ce7915635d25b5600a162dd6.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/jps/static/js/runtime~main.5e676a65.js"
   },
   {
-    "revision": "a8eeae0061fa42adaef4",
-    "url": "/jps/static/js/main.4cbb8bf2.chunk.js"
+    "revision": "5a7cf5e0cab240f5320e",
+    "url": "/jps/static/js/main.cfdc5c74.chunk.js"
   },
   {
     "revision": "9ff9e18c9070ad465a54",
     "url": "/jps/static/js/2.183242a3.chunk.js"
   },
   {
-    "revision": "a8eeae0061fa42adaef4",
+    "revision": "5a7cf5e0cab240f5320e",
     "url": "/jps/static/css/main.985e21c7.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/jps/static/css/2.0b813455.chunk.css"
   },
   {
-    "revision": "aeb0952c8cf4e55e0be73e4a2f184ed8",
+    "revision": "840bb85f46d8bba41b4bcb762e685958",
     "url": "/jps/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/jps/precache-manifest.67f6f77993533e2bfac57725c5790306.js"
+  "/jps/precache-manifest.85d8e487ce7915635d25b5600a162dd6.js"
 );
 
 workbox.clientsClaim();
