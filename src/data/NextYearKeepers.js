25910d6cf1e7c374e009b45336ec013aaf405b7f
Matt Wilkens <matthew.j.wilkens@gmail.com>
2022-03-26 09:52:28 -0500

    2022 keepers

--- src/data/NextYearKeepers.js
+++ src/data/NextYearKeepers.js
@@ -1,89 +1,85 @@
 export default [
   //pork
-  // {  name: 'gurriel', cost: 9, year: 2, playerId: '10616' },
-  // {  name: 'price', cost: 19, year: 2, playerId: '8175' },
-  // {  name: 'Kopech', cost: 23, year: 3, playerId: '10412' },
-  // {  name: 'montas', cost: 23, year: 3, playerId: '10027'},
-  // {  name: 'burnes', cost: 26, year: 2, playerId: '10910'},
-  // {  name: 'mountcastle', cost: 26, year: 2, playerId: '10429'},
-  // {  name: 'santander', cost: 26, year: 2, playerId: '10666'},
-  //
-  // //cleverness
-  //
-  // //uribe
-  // {  name: 'degrom', cost: 1, year: 2, playerId: '9701' },
-  // {  name: 'paddack', cost: 13, year: 3, playerId: '10575' },
-  // {  name: 'alonso', cost: 14, year: 3, playerId: '10918' },
-  // {  name: 'verdugo', cost: 17, year: 2, playerId: '10505'},
-  // {  name: 'biggio', cost: 23, year: 3, playerId: '11086'},
-  // {  name: 'bassitt', cost: 26, year: 2, playerId: '9799'},
-  // {  name: 'lopez', cost: 26, year: 2, playerId: '11057'},
-  //
-  // //shenanigans
-  // {  name: 'carlson', cost: 15, year: 2, playerId: '10643' },
-  // {  name: 'karinchak', cost: 26, year: 2, playerId: '11632' },
-  // {  name: 'lynn', cost: 23, year: 3, playerId: '8650' },
-  // {  name: 'gallen', cost: 23, year: 3, playerId: '11116'},
-  // {  name: 'happ', cost: 26, year: 2, playerId: '10440'},
-  // {  name: 'lewis', cost: 26, year: 2, playerId: '10584'},
-  // {  name: 'yastrzemski', cost: 26, year: 2, playerId: '10119'},
-  //
-  // //pollocks
-  // {  name: 'bryant', cost: 1, year: 2, playerId: '9558' },
-  // {  name: 'albies', cost: 1, year: 2, playerId: '10237' },
-  // {  name: 'chapman', cost: 2, year: 2, playerId: '8616' },
-  // {  name: 'hendricks', cost: 4, year: 2, playerId: '9758'},
-  // {  name: 'seager', cost: 5, year: 2, playerId: '9584'},
-  // {  name: 'sale', cost: 18, year: 2, playerId: '8780'},
-  // {  name: 'lemahieu', cost: 20, year: 3, playerId: '8949'},
-  //
-  // //vasta
-  // {  name: 'bellinger', cost: 1, year: 2, playerId: '10504' },
-  // {  name: 'kershaw', cost: 3, year: 3, playerId: '8180' },
-  // {  name: 'bieber', cost: 5, year: 3, playerId: '10867' },
-  // {  name: 'bummer', cost: 21, year: 2, playerId: '10773'},
-  // {  name: 'hosmer', cost: 26, year: 2, playerId: '8857'},
-  // {  name: 'staumont', cost: 26, year: 2, playerId: '10492'},
-  // {  name: 'grisham', cost: 26, year: 2, playerId: '10522'},
-  //
-  // //mcshrinkynuts
-  // {  name: 'didi', cost: 17, year: 3, playerId: '9282'},
-  // {  name: 'franco', cost: 21, year: 2, playerId: '11573'},
-  // {  name: 'urias', cost: 23, year: 3, playerId: '9585'},
-  // {  name: 'rosenthal', cost: 26, year: 2, playerId: '9240'},
-  //
-  // //dubya
-  // {  name: 'trout', cost: 1, year: 2, playerId: '8861' },
-  // {  name: 'tucker', cost: 10, year: 2, playerId: '10480' },
-  // {  name: 'Luzardo', cost: 15, year: 3, playerId: '10627' },
-  // {  name: 'lamet', cost: 23, year: 3, playerId: '10703'},
-  // {  name: 'perez', cost: 22, year: 3, playerId: '9015'},
-  // {  name: 'tatis', cost: 23, year: 3, playerId: '10639'},
-  // {  name: 'cronenworth', cost: 26, year: 2, playerId: '11692'},
-  //
-  // //slump
-  // {  name: 'cole', cost: 1, year: 2, playerId: '9121' },
-  // {  name: 'woodruff', cost: 2, year: 2, playerId: '10730' },
-  // {  name: 'devers', cost: 5, year: 3, playerId: '10235' },
-  // {  name: 'soler', cost: 18, year: 3, playerId: '9228'},
-  // {  name: 'cease', cost: 23, year: 3, playerId: '10438'},
-  //
-  // //topes
-  // {  name: 'glasnow', cost: 7, year: 3, playerId: '9616' },
-  // {  name: 'voit', cost: 11, year: 2, playerId: '10745' },
-  // {  name: 'giolito', cost: 23, year: 3, playerId: '9640' },
-  // {  name: 'hiura', cost: 23, year: 3, playerId: '10912'},
-  // {  name: 'bichette', cost: 23, year: 3, playerId: '10766'},
-  // {  name: 'sixto', cost: 26, year: 2, playerId: '10932'},
-  // {  name: 'dom smith', cost: 26, year: 2, playerId: '9595'},
-  //
-  // //smalls
-  // {  name: 'turner', cost: 1, year: 2, playerId: '10056' },
-  // {  name: 'soroka', cost: 23, year: 3, playerId: '10423' },
-  // {  name: 'fried', cost: 23, year: 3, playerId: '9620' },
-  // {  name: 'robert', cost: 23, year: 3, playerId: '10765'},
-  // {  name: 'williams', cost: 26, year: 2, playerId: '11628'},
-  // {  name: 'plesac', cost: 26, year: 2, playerId: '11590'},
-  // {  name: 'bohm', cost: 26, year: 2, playerId: '11365'}
+  {  name: 'olson', cost: 6, year: 1, playerId: '9605' },
+  {  name: 'mountcastle', cost: 25, year: 2, playerId: '10429' },
+  {  name: 'rodgers', cost: 25, year: 1, playerId: '10230' },
+  {  name: 'schwindel', cost: 25, year: 1, playerId: '11105'},
+  {  name: 'burnes', cost: 25, year: 2, playerId: '10910'},
+  {  name: 'gausman', cost: 10, year: 1, playerId: '9334'},
+  {  name: 'gilbert', cost: 25, year: 1, playerId: '11381'},
+
+  //ur mom
+
+  //josh
+
+  //uribe
+
+  //shenanigans
+  {  name: 'mullins', cost: 25, year: 1, playerId: '10849' },
+  {  name: 'oneill', cost: 25, year: 1, playerId: '10215' },
+  {  name: 'rutschman', cost: 25, year: 1, playerId: '11732' },
+  {  name: 'manoah', cost: 25, year: 1, playerId: '11854'},
+  {  name: 'mcclanahan', cost: 25, year: 1, playerId: '11398'},
+  {  name: 'clase', cost: 25, year: 2, playerId: '11625'},
+  {  name: 'garcia', cost: 25, year: 1, playerId: '11903'},
+
+  //pollocks
+  {  name: 'schwarber', cost: 9, year: 1, playerId: '9861' },
+  {  name: 'albies', cost: 1, year: 2, playerId: '10237' },
+  {  name: 'swanson', cost: 8, year: 1, playerId: '10234' },
+  {  name: 'pressly', cost: 10, year: 1, playerId: '9358'},
+  {  name: 'seager', cost: 5, year: 2, playerId: '9584'},
+  {  name: 'sale', cost: 18, year: 2, playerId: '8780'},
+
+  //vasta
+  {  name: 'harper', cost: 1, year: 1, playerId: '8875' },
+  {  name: 'ohtani', cost: 25, year: 1, playerId: '1000001' },
+  {  name: 'correa', cost: 11, year: 1, playerId: '9573' },
+  {  name: 'witt', cost: 23, year: 1, playerId: '11771'},
+  {  name: 'graveman', cost: 25, year: 1, playerId: '9821'},
+  {  name: 'bummer', cost: 21, year: 2, playerId: '10773'},
+  {  name: 'crochet', cost: 22, year: 1, playerId: '12046'},
+
+  //mcshrinkynuts
+  {  name: 'soto', cost: 1, year: 1, playerId: '10626'},
+  {  name: 'franco', cost: 21, year: 2, playerId: '11573'},
+  {  name: 'polanco', cost: 20, year: 1, playerId: '9750'},
+  {  name: 'canha', cost: 24, year: 1, playerId: '9898'},
+
+  //dubya
+  {  name: 'vlad', cost: 3, year: 1, playerId: '10621' },
+  {  name: 'tucker', cost: 10, year: 2, playerId: '10480' },
+  {  name: 'semien', cost: 9, year: 1, playerId: '9517' },
+  {  name: 'hendriks', cost: 4, year: 1, playerId: '9057'},
+  {  name: 'ray', cost: 24, year: 1, playerId: '9691'},
+  {  name: 'verlander', cost: 25, year: 1, playerId: '7590'},
+  {  name: 'bassitt', cost: 25, year: 2, playerId: '9799'},
+
+  //slump
+  {  name: 'haniger', cost: 21, year: 1, playerId: '10369' },
+  {  name: 'vaughn', cost: 24, year: 1, playerId: '11866' },
+  {  name: 'wisdom', cost: 25, year: 1, playerId: '11093' },
+  {  name: 'sheets', cost: 25, year: 1, playerId: '10879'},
+  {  name: 'hader', cost: 4, year: 1, playerId: '10131'},
+  {  name: 'rodon', cost: 25, year: 1, playerId: '9823'},
+  {  name: 'g soto', cost: 25, year: 1, playerId: '10882'},
+
+  //topes
+  {  name: 'chisholm', cost: 25, year: 1, playerId: '10839' },
+  {  name: 'edman', cost: 12, year: 1, playerId: '11596' },
+  {  name: 'varsho', cost: 18, year: 1, playerId: '10843' },
+  {  name: 'ohtani p', cost: 8, year: 1, playerId: '1000002'},
+  {  name: 'gallegos', cost: 15, year: 1, playerId: '10700'},
+  {  name: 'romano', cost: 25, year: 1, playerId: '11253'},
+  {  name: 'baz', cost: 25, year: 1, playerId: '10934'},
+
+  //smalls
+  {  name: 'freeman', cost: 1, year: 1, playerId: '8658' },
+  {  name: 'riley', cost: 10, year: 1, playerId: '10420' },
+  {  name: 'taylor', cost: 18, year: 1, playerId: '9771' },
+  {  name: 'france', cost: 25, year: 1, playerId: '11231'},
+  {  name: 'peralta', cost: 19, year: 1, playerId: '11014'},
+  {  name: 'wainwright', cost: 25, year: 1, playerId: '7048'},
+  {  name: 'gibson', cost: 25, year: 1, playerId: '8864'}
 
 ];
