582d5e977d4da962b15c7da49823162e8ddaaf41
Matt Wilkens <matthew.j.wilkens@gmail.com>
2023-03-11 12:32:55 -0600

    2023 keepers

--- src/data/AppData.js
+++ src/data/AppData.js
@@ -5,21 +5,22 @@ export default {
   clientSecret: '4a1c4d6e5af02bfe69e226d666a5113a7b2b35a6',
   corsAnywhereUrl: 'https://cors-anywhere-jps.onrender.com/',
   leagueKeys: {
-    lastYear: '404.l.8715',
-    thisYear: '412.l.33535'
+    lastYear: '412.l.33535',
+    thisYear: '422.l.22971'
   },
   lastYearTeamKeys: {
     1: '.t.1', //Pork
     2: '.t.2',  //Anal
     3: '.t.3', //Angry
-    4: '.t.5', //dubya
-    5: '.t.6', //vasta
-    6: '.t.7', //uribe
-    7: '.t.9', //shenanigans
-    8: '.t.10', //slump
-    9: '.t.11', //gary
-    10: '.t.12', //joe
-    11: '.t.4' //lucas
+    4: '.t.9', //craw
+    5: '.t.4', //dubya
+    6: '.t.5', //bros
+    7: '.t.12', //firstrate
+    8: '.t.6', //uribe
+    9: '.t.7', //shenanigans
+    10: '.t.8', //slump
+    11: '.t.10', //smalls,
+    12: '.t.11' //mom
   },
   roundsInDraft: 25
 };
